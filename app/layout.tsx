7a8cb44e3d2ca4352644b96e79e2e11278fb64cd
dev1gottia <g39101104@gmail.com>
2025-11-17 19:32:24 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,7 @@ export default function RootLayout({
                 suppressHydrationWarning>
 
       <head>
-        <script id="_waug5o">var _wau = _wau || []; _wau.push(["classic", "livetb", "g5o"]);</script><script async src="//waust.at/c.js"></script>
-         <script type='text/javascript' src='//reclinecostumepessimistic.com/a6/9d/77/a69d77953fe342a7a7c2b77b30b156df.js'></script>
+        <script id="_waug5o">var _wau = _wau || []; _wau.push(["classic", "liveab", "g5o"]);</script><script async src="//waust.at/c.js"></script>
       </head>
       
       <body>
